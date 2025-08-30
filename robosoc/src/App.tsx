8e6888c534d60a88f18930e4cc4f2bc3f14e9587
Areen_Sharma <sharmaareen29@gmail.com>
2025-08-30 10:30:50 +0530

    Version 30.08.2025

---
+++ robosoc/src/App.tsx
@@ -0,0 +1,40 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/header'
+import Footer from './components/footer'
+import SmoothScrollProvider from './components/SmoothScrollProvider'
+import Home from './home'
+import AboutUs from './aboutus'
+import Projects from './projects'
+import Members from './members'
+import Achievements from './achievement'
+import Inventory from './inventory'
+import ContactUs from './contactus'
+import AdminPanel from './admin'
+
+
+function App() {
+  return (
+    <SmoothScrollProvider>
+      <div className="App">
+        <Header />
+        <main className="md:ml-0 ml-16 transition-all duration-300">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/members" element={<Members />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/Campaign's" element={<Inventory />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/admin" element={<AdminPanel />} />
+          </Routes>
+        </main>
+        <div className="md:ml-0 ml-16 transition-all duration-300 relative z-30">
+          <Footer />
+        </div>
+      </div>
+    </SmoothScrollProvider>
+  )
+}
+
+export default App
